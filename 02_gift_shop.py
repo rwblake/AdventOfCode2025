6f2e04f468713a75af9dd07fe374b6113019fe6f
Robert Blake <rwblake04@gmail.com>
2025-12-02 10:53:19 +0000

    day 2 part 2

--- 02_gift_shop.py
+++ 02_gift_shop.py
@@ -23,8 +23,33 @@ def part_one(lines):
 
 
 def part_two(lines):
-	pass
+	# now invalid ids are just repeated sequences
+	
+	id_ranges = lines[0].split(',')
+	total = 0
+	
+	for id_range in id_ranges:
+		first_id, last_id = id_range.split('-')
+		first_id, last_id = int(first_id), int(last_id)
 
+		for pid in range(first_id, last_id + 1):
+			pid = str(pid)
+
+			# iterate over number of divides in the string
+			# i.e. # of repeated sequences
+			for divisor in range(2, len(pid) + 1):
+				# eliminate strings that don't divide evenly
+				if len(pid) % divisor != 0:
+					continue
+
+				# split pid into equal parts
+				quotient = len(pid) // divisor
+				parts = [pid[i * quotient : (i + 1) * quotient] for i in range(divisor)]
+				# check if they're all the same
+				if all([parts[0] == part for part in parts[1:]]):
+					total += int(pid)
+					break  # count each invalid pid only once
+	return total
 
 def main():
 	standard_path = "input/02_gift_shop.txt"
