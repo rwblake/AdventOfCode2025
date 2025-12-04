71811d396e5dd2ff54a2651eb4d6406ab0299089
Robert Blake <rwblake04@gmail.com>
2025-12-04 20:47:37 +0000

    day 4 part 2

--- 04_printing_department.py
+++ 04_printing_department.py
@@ -12,31 +12,59 @@ def get(x, y, grid):
 	return grid[y][x]
 
 
+def remove(x, y, grid):
+	row = list(grid[y])
+	row[x] = '.'
+	grid[y] = ''.join(row)
+
+
 def neighbours(x, y, width, height):
 	neighbours = [[x+dx, y+dy] for dx, dy in DIRECTIONS if 0 <= x+dx < width and 0 <= y+dy < height]
 	return neighbours
+
+
+def accessible(x, y, grid):
+	width, height = len(grid[0]), len(grid)
+	ns = neighbours(x, y, width, height)
+	count = sum([get(nx, ny, grid) == PAPER for nx, ny in ns])
+	return count < 4
 
 
 def part_one(grid):
 	width = len(grid[0])
 	height = len(grid)
 
-	accessible = 0
+	num_accessible = 0
 	for x in range(width):
 		for y in range(height):
 			if get(x, y, grid) != PAPER:
 				continue
 
-			ns = neighbours(x, y, width, height)
-			count = sum([get(nx, ny, grid) == PAPER for nx, ny in ns])
-			if count < 4:
-				accessible += 1
-	return accessible
+			if accessible(x, y, grid):
+				num_accessible += 1
+	return num_accessible
 
 
-def part_two(lines):
-	pass
+def part_two(grid):
+	width = len(grid[0])
+	height = len(grid)
 
+	removed_total = 0
+	while True:
+		removed = 0
+		for x in range(width):
+			for y in range(height):
+				if get(x, y, grid) != PAPER:
+					continue
+
+				if accessible(x, y, grid):
+					remove(x, y, grid)
+					removed += 1
+		removed_total += removed
+
+		if removed == 0:
+			break
+	return removed_total
 
 def main():
 	standard_path = "input/04_printing_department.txt"
